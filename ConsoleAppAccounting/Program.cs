ef99dc0e084a80361acbc4ff0b444bf481ec019b
student <student@learncodinganywhere.com>
2020-12-01 20:00:03 -0800

    Add project files.

---
+++ ConsoleAppAccounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ConsoleAppAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Acme Accounting Systems \nRemember, We're \"accouting\" on you! ");
+            Console.ReadLine();
+        }
+    }
+}
